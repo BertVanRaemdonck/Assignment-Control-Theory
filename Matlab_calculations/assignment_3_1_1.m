5e25c4f849fac18f6524e9d1a708ea749837f24d
BertVanRaemdonck <bert1.vanraemdonck@telenet.be>
2016-11-04 10:19:22 +0100

    Conversie data naar frequentiedomein + bodeplots

--- Matlab_calculations/assignment_3_1_1.m
+++ Matlab_calculations/assignment_3_1_1.m
@@ -3,37 +3,42 @@ clear all
 clc
 clear
 
-rec_random1 = readlog('log_gpio_ramp2.xml');
+%% Parameters
+fs = 100;   % Hz
+Ts = 1/fs;  % s
 
-random1_t = rec_random1.getData('time');
-random1_v = rec_random1.getData('Voltage');
-random1_enc1 = rec_random1.getData('Encoder1');
-random1_enc2 = rec_random1.getData('Encoder2');
+%% Viewing data
+rec_random1 = readlog('log_gpio_random1.xml');
+
+t = rec_random1.getData('time');
+v = rec_random1.getData('Voltage');
+enc1 = rec_random1.getData('Encoder1');
+enc2 = rec_random1.getData('Encoder2');
 
 figure()
 subplot(3,1,1)
-plot(random1_t, random1_v);
+plot(t, v);
 subplot(3,1,2)
-plot(random1_t, random1_enc1);
+plot(t, enc1);
 subplot(3,1,3)
-plot(random1_t, random1_enc2);
+plot(t, enc2);
 
 
 
 %% Unwrapping the values of the encoder
 
 tol_encoder = 32761;
-random1_enc1_a = random1_enc1;
-for i = length(random1_enc1)-1:-1:1
+random1_enc1_a = enc1;
+for i = length(enc1)-1:-1:1
     
-    Y = sign(random1_enc1);
+    Y = sign(enc1);
     if Y(i)==1 && Y(i+1)==-1
-        if random1_enc1(i) > 32700
+        if enc1(i) > 32700
             random1_enc1_a(i+1:end) = random1_enc1_a(i+1:end) + (2*tol_encoder);
         end
     end
     if Y(i)==-1 && Y(i+1)==1
-        if random1_enc1(i) < -32700
+        if enc1(i) < -32700
             random1_enc1_a(i+1:end) = random1_enc1_a(i+1:end) - (2*tol_encoder);
         end
     end
@@ -41,24 +46,24 @@ end
 
 figure()
 subplot(3,1,1)
-plot(random1_t, random1_enc1_a);
+plot(t, random1_enc1_a);
 subplot(3,1,2)
-plot(random1_t, random1_enc1);
+plot(t, enc1);
 subplot(3,1,3)
-plot(random1_t, random1_enc1_a-random1_enc1);
+plot(t, random1_enc1_a-enc1);
 
 
-random1_enc2_a = random1_enc2;
-for i = length(random1_enc2)-1:-1:1
+random1_enc2_a = enc2;
+for i = length(enc2)-1:-1:1
     
-    Y = sign(random1_enc2);
+    Y = sign(enc2);
     if Y(i)==1 && Y(i+1)==-1
-        if random1_enc2(i) > 32700
+        if enc2(i) > 32700
             random1_enc2_a(i+1:end) = random1_enc2_a(i+1:end) + (2*tol_encoder);
         end
     end
     if Y(i)==-1 && Y(i+1)==1
-        if random1_enc2(i) < -32700
+        if enc2(i) < -32700
             random1_enc2_a(i+1:end) = random1_enc2_a(i+1:end) - (2*tol_encoder);
         end
     end
@@ -66,9 +71,27 @@ end
 
 figure()
 subplot(3,1,1)
-plot(random1_t, random1_enc2_a);
+plot(t, random1_enc2_a);
 subplot(3,1,2)
-plot(random1_t, random1_enc2);
+plot(t, enc2);
 subplot(3,1,3)
-plot(random1_t, random1_enc2_a-random1_enc2);
+plot(t, random1_enc2_a-enc2);
+
+%% Converting to frequency domain
+n = size(t,1);
+f = fs*(-n/2:n/2-1)/n;
+v_f = fftshift(fft(v))/n;
+enc1_f = fftshift(fft(enc1))/n;
+
+figure()
+subplot(2,1,1)
+plot(f, abs(v_f))
+subplot(2,1,2)
+plot(f, unwrap(angle(v_f)))
+
+figure()
+subplot(2,1,1)
+semilogx(f, 20*log10(abs(enc1_f./v_f)))
+subplot(2,1,2)
+semilogx(f, 180/pi*unwrap(angle(enc1_f./v_f)))
         
