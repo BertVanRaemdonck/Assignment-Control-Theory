7bddd77ed8811f6cfd8e808b62cce1e2ac7dfb56
BenVandenBroeck <4d428b0a@opayq.com>
2016-11-18 17:17:25 +0100

    Opkuisen code + berekeningen gekopieerd voor encoder 2
    * Code is nu overzichtelijker
    	--> alle uitgecommentarieerde code is weggedaan
    	--> extra uitleg bij bepaalde parameters geschreven
    * Controller van encoder 2 wordt berekend
    	==> TOTAAL ANDERS DAN BIJ ENCODER 1 !!!!
    		--> andere overgangsfrequentie
    		--> andere fase marge
    		--> totaal andere transferfunctie
    * Nu wordt het ook omgezet naar discrete tijd
    	--> geprint en geplot
    	--> transferfunctie discrete tijd wordt berekend en uitgeprint

--- Matlab_calculations/assignment_3_1_2.m
+++ Matlab_calculations/assignment_3_1_2.m
@@ -8,7 +8,7 @@ assignment_3_1_1
 
 
 
-%% Code oefenzitting 4
+%% Parameters, getting data and calculations
 
 close all
 clc
@@ -16,6 +16,12 @@ clc
 % parameters
 delta_phi = 45;  %graden
 add_phase_lag = 15; %graden
+low_exp_w = -1;     % w = logspace(low_exp_w,high_exp_w,number_points_w)
+high_exp_w = 3;
+number_points_w = 1000;
+
+PM_des = 55;        % desired phase marge
+phase_PI = 40;      % phase that PI controller may 'eat'
 
 % Taking data from assignment_3_1_1.m
 num_enc1_or = num_enc1;
@@ -26,117 +32,78 @@ den_enc2_or = den_enc2;
 sys_enc1_or = tf(num_enc1_or,den_enc1_or,Ts);
 sys_enc2_or = tf(num_enc2_or,den_enc2_or,Ts);
 
-% figure('name','Bodeplot of encoders without PI DISCRETE TIME')
-% subplot(2,1,1)
-% bode(sys_enc1_or),grid
-% subplot(2,1,2)
-% bode(sys_enc2_or),grid
-
 sys_enc1_c = d2c(sys_enc1_or);    % We have to work in continuous time
 sys_enc2_c = d2c(sys_enc2_or);
-
-% num_enc1_or = cell2mat(sys_enc1_or.num);
-% den_enc1_or = cell2mat(sys_enc1_or.den);
-% 
-% probeersel = tf(num_enc1_or,den_enc1_or);
 
 figure('name','Bodeplot of encoders without PI in discrete and continuous time')
 subplot(2,1,1)
 bode(sys_enc1_or, sys_enc1_c),grid
+legend('enc1 disc', 'enc1 cont')
 subplot(2,1,2)
 bode(sys_enc2_or, sys_enc2_c),grid
+legend('enc2 disc', 'enc2 cont')
 
-% figure('name','probeersel CONTINUOUS TIME')
-% bode(sys_enc1_or),grid
-% hold on
-% bode(probeersel),grid
-% hold off
+% calculating magnitude and phase for needed calculations
+w = logspace(low_exp_w,high_exp_w,number_points_w);
 
-% % other method of making bodeplot
-w1 = logspace(-1,3,1000);
-%[mag1_or,ph1_or,w1] = bode(sys_enc1_or);
-[mag1_c,ph1_c] = bode(sys_enc1_c.num, sys_enc1_c.den, w1);
-%[mag2_or,ph2_or,w2] = bode(sys_enc2_or);
-[mag2_or,ph2_or] = bode(num_enc2_or,den_enc2_or,w1);
-% 
-% mag1_or = squeeze(mag1_or);       %bode returns a 3D array, squeeze returns array without array with dimension 1
-% mag2_or = squeeze(mag2_or); 
-% ph1_or = squeeze(ph1_or); 
-% ph2_or = squeeze(ph2_or);
-% 
-% figure('name','Bodeplot of encoder1 without PI using calculations')
-% subplot(2,1,1)
-% loglog(w1,mag1_or),grid
-% subplot(2,1,2)
-% semilogx(w1,ph1_or),grid
+[mag1_c,ph1_c] = bode(sys_enc1_c.num, sys_enc1_c.den, w);
+[mag2_c,ph2_c] = bode(sys_enc2_c.num, sys_enc2_c.den, w);
 
-% design procedure PI encoder 1
 
-PM_des = 55;
-phase_PI = 40;
 
-phase_co1 = -180 + PM_des + phase_PI;
-omega_co1 = interp1(ph1_c, w1, phase_co1);
+%% design procedure PI encoder 1
 
-T_i1 = tan(pi/180*(90 - phase_PI))/omega_co1;
-mag_PI_co1 = abs(1/(j*omega_co1)*(j*omega_co1+1/T_i1));
-K_1 = 1/(mag_PI_co1 * interp1(w1, mag1_c, omega_co1));
+phase_co1 = -180 + PM_des + phase_PI;       % phase we want
+omega_co1 = interp1(ph1_c, w, phase_co1);  % frequency with phase we want, will become new crossover frequency
 
-num_PI = [K_1*T_i1 K_1];
-den_PI = [T_i1 0];
-sys_PI = tf(num_PI, den_PI);
+T_i1 = tan(pi/180*(90 - phase_PI))/omega_co1;  % integrated time constant of PI encoder 1
+mag_PI_co1 = abs(1/(j*omega_co1)*(j*omega_co1+1/T_i1));  % magnitude of PI controller at crossover frequency
+K_1 = 1/(mag_PI_co1 * interp1(w, mag1_c, omega_co1));   % proportional constant of PI controller encoder 1
 
-figure('name', 'Bodeplot of the PI compensator')
-bode(sys_PI, w1), grid
+num_PI1 = [K_1*T_i1 K_1];        % D(s) = (K * T_i * s + K)/(T_i * s)
+den_PI1 = [T_i1 0];
+sys_PI1 = tf(num_PI1, den_PI1);    
 
-sys_enc1_PIcomp = series(sys_PI, sys_enc1_c);
+figure('name', 'Bodeplot of the PI compensator encoder 1')
+bode(sys_PI1, w), grid
 
-figure('name', 'Bodeplot of the compensated system')
-bode(sys_enc1_c, sys_enc1_PIcomp, w1), grid
+sys_enc1_PIcomp_c = series(sys_PI1, sys_enc1_c);
+sys_enc1_PIcomp_d = c2d(sys_enc1_PIcomp_c,Ts)
 
-% phi_1 = -(180 - delta_phi - add_phase_lag)
-% omega_co1 = w1(find(ph1_or < phi_1, 1))
-% %omega_co1 = interp1(ph1_or,w1,phi_1)
-% 
-% T_i1 = 1/(omega_co1*tan(add_phase_lag * pi /180))
-% 
-% num_I = [T_i1,1];
-% den_I = [T_i1,0];
-% 
-% sys_I = tf(num_I,den_I);
-% 
-% figure('name','Bodeplot integrator I')
-% bode(sys_I)
-% 
-% num_enc1_I = conv(num_enc1_or,num_I);
-% den_enc1_I = conv(den_enc1_or,den_I);
-% 
-% sys_enc1_I = tf(num_enc1_I,den_enc1_I);
-% 
-% [mag1_I,ph1_I,w1_I] = bode(sys_enc1_I);
-% 
-% mag1_I = squeeze(mag1_I); 
-% ph1_I = squeeze(ph1_I); 
-% 
-% figure('name','Bodeplot encoder 1 with I')
-% bode(sys_enc1_I)
-% 
-% % omega_co1_I = interp1(mag1_I,w1_I,1)
-% mag1_omega_co = interp1(w1_I,mag1_I,omega_co1)
-% 
-% k_I = 1/mag1_omega_co
-% 
-% 
-% % Calculating PI controller
-% num_enc1_PI = k_I*num_enc1_I;
-% den_enc1_PI = den_enc1_I;
-% 
-% sys_enc1_PI = tf(num_enc1_PI,den_enc1_PI);
-% 
-% [mag1_PI,ph1_PI,w1_PI] = bode(sys_enc1_PI);
-% 
-% mag1_PI = squeeze(mag1_PI); 
-% ph1_PI = squeeze(ph1_PI); 
-% 
-% figure('name','Bodeplot encoder 1 with PI')
-% bode(sys_enc1_PI),grid
+figure('name', 'Bodeplot of the compensated system of encoder 1 continuous time')
+bode(sys_enc1_c, sys_enc1_PIcomp_c, w), grid
+legend('enc1', 'enc1_PIcomp')
+
+figure('name', 'Bodeplot of the compensated system of encoder 1 discrete time')
+bode(sys_enc1_or, sys_enc1_PIcomp_d, w), grid
+legend('enc1', 'enc1_PIcomp')
+
+
+
+%% design procedure PI encoder 2
+
+phase_co2 = -180 + PM_des + phase_PI;       % phase we want
+omega_co2 = interp1(ph2_c, w, phase_co2);  % frequency with phase we want, will become new crossover frequency
+
+T_i2 = tan(pi/180*(90 - phase_PI))/omega_co2;  % integrated time constant of PI encoder 2
+mag_PI_co2 = abs(1/(j*omega_co2)*(j*omega_co2+1/T_i2));  % magnitude of PI controller at crossover frequency
+K_2 = 1/(mag_PI_co2 * interp1(w, mag2_c, omega_co2));   % proportional constant of PI controller encoder 2
+
+num_PI2 = [K_2*T_i2 K_2];        % D(s) = (K * T_i * s + K)/(T_i * s)
+den_PI2 = [T_i2 0];
+sys_PI2 = tf(num_PI2, den_PI2);    
+
+figure('name', 'Bodeplot of the PI compensator encoder 2')
+bode(sys_PI2, w), grid
+
+sys_enc2_PIcomp_c = series(sys_PI2, sys_enc2_c);
+sys_enc2_PIcomp_d = c2d(sys_enc2_PIcomp_c,Ts)
+
+figure('name', 'Bodeplot of the compensated system of encoder 2 continuous time')
+bode(sys_enc2_c, sys_enc2_PIcomp_c, w), grid
+legend('enc2', 'enc2_PIcomp')
+
+figure('name', 'Bodeplot of the compensated system of encoder 2 discrete time')
+bode(sys_enc2_or, sys_enc2_PIcomp_d, w), grid
+legend('enc2', 'enc2_PIcomp')
+
