a10c4d133a1536a55186e51dd78a06f0ebd57f50
BertVanRaemdonck <bert1.vanraemdonck@telenet.be>
2016-11-21 12:37:15 +0100

    Andere soort parameters voor controller
    * Om de controller te ontwerpen geef je nu eenvoudig de gewenste crossover en fasemarge in in plaats van een obscure grootheid. De resultaten zijn logisch: je kan mooi met beide waarden spelen en zien wat voor effect ze hebben op de output
    * Paar cosmetische veranderingen

--- Matlab_calculations/assignment_3_1_2.m
+++ Matlab_calculations/assignment_3_1_2.m
@@ -24,10 +24,11 @@ speed_ref = (mag_speed_ref/4)*ones([1,nb_speed_ref/3]);
 speed_ref = [speed_ref  (mag_speed_ref/2)*ones([1,nb_speed_ref/3])];
 speed_ref = [speed_ref  (mag_speed_ref/1)*ones([1,nb_speed_ref/3])];
 
-PM_des1 = 55;        % desired phase marge of controller 1      original was 55
-phase_PI1 = 55;      % phase that PI controller 1 may 'eat'     original was 40
-PM_des2 = 55;        % desired phase marge of controller 2      original was 55
-phase_PI2 = 40;      % phase that PI controller 2 may 'eat'     original was 40
+PM1 = 55;           % desired phase marge of controller 1      original was 55
+omega_co1 = 30;     % desired crossover frequency in rad/s
+%phase_PI1 = 55;     % phase that PI controller 1 may 'eat'     original was 40
+PM2 = 55;           % desired phase marge of controller 2      original was 55
+phase_PI2 = 40;     % phase that PI controller 2 may 'eat'     original was 40
 
 % Taking data from assignment_3_1_1.m
 num_enc1_or = num_enc1;
@@ -59,11 +60,13 @@ w = logspace(low_exp_w,high_exp_w,number_points_w);
 
 %% design procedure PI encoder 1
 
-phase_co1 = -180 + PM_des1 + phase_PI1;       % phase we want
-omega_co1 = interp1(ph1_c, w, phase_co1);  % frequency with phase we want, will become new crossover frequency
+%phase_co1 = -180 + PM_des1 + phase_PI1;     % phase we want
+%omega_co1 = interp1(ph1_c, w, phase_co1);   % frequency with phase we want, will become new crossover frequency
 
-T_i1 = tan(pi/180*(90 - phase_PI1))/omega_co1;  % time constant of PI encoder 1
-mag_PI_co1 = abs(1/(j*omega_co1)*(j*omega_co1+1/T_i1));  % magnitude of PI controller at crossover frequency
+phase_PI1 = -(-180 + PM1 - interp1(w, ph1_c, omega_co1));   % phase of the PI controller at the crossover frequency
+
+T_i1 = tan(pi/180*(90 - phase_PI1))/omega_co1;          % time constant of PI encoder 1
+mag_PI_co1 = abs(1/(j*omega_co1)*(j*omega_co1+1/T_i1)); % magnitude of PI controller at crossover frequency
 K_1 = 1/(mag_PI_co1 * interp1(w, mag1_c, omega_co1));   % proportional constant of PI controller encoder 1
 
 num_PI1 = [K_1*T_i1 K_1];        % D(s) = (K * T_i * s + K)/(T_i * s)
@@ -73,6 +76,7 @@ sys_PI1 = tf(num_PI1, den_PI1);
 figure('name', 'Bodeplot of the PI compensator encoder 1')
 bode(sys_PI1, w), grid
 
+% Viewing resulting system
 sys_enc1_PIcomp_c = series(sys_PI1, sys_enc1_c);
 sys_enc1_PIcomp_d = c2d(sys_enc1_PIcomp_c,Ts)
 
@@ -88,11 +92,11 @@ legend('enc1', 'enc1_PIcomp')
 
 %% design procedure PI encoder 2
 
-phase_co2 = -180 + PM_des2 + phase_PI2;       % phase we want
-omega_co2 = interp1(ph2_c, w, phase_co2);  % frequency with phase we want, will become new crossover frequency
+phase_co2 = -180 + PM2 + phase_PI2;     % phase we want
+omega_co2 = interp1(ph2_c, w, phase_co2);   % frequency with phase we want, will become new crossover frequency
 
-T_i2 = tan(pi/180*(90 - phase_PI2))/omega_co2;  % time constant of PI encoder 2
-mag_PI_co2 = abs(1/(j*omega_co2)*(j*omega_co2+1/T_i2));  % magnitude of PI controller at crossover frequency
+T_i2 = tan(pi/180*(90 - phase_PI2))/omega_co2;          % time constant of PI encoder 2
+mag_PI_co2 = abs(1/(j*omega_co2)*(j*omega_co2+1/T_i2)); % magnitude of PI controller at crossover frequency
 K_2 = 1/(mag_PI_co2 * interp1(w, mag2_c, omega_co2));   % proportional constant of PI controller encoder 2
 
 num_PI2 = [K_2*T_i2 K_2];        % D(s) = (K * T_i * s + K)/(T_i * s)
@@ -102,6 +106,7 @@ sys_PI2 = tf(num_PI2, den_PI2);
 figure('name', 'Bodeplot of the PI compensator encoder 2')
 bode(sys_PI2, w), grid
 
+% Viewing resulting system
 sys_enc2_PIcomp_c = series(sys_PI2, sys_enc2_c);
 sys_enc2_PIcomp_d = c2d(sys_enc2_PIcomp_c,Ts)
 
