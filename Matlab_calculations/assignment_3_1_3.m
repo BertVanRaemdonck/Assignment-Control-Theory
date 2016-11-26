da0b6b1b7809a7e478dd860dc10892fb77ee2226
BenVandenBroeck <4d428b0a@opayq.com>
2016-11-26 15:03:52 +0100

    * Aanpassingen: 	* Assignment_3_1_2.m 		- Plot nu ook alleen de figuren als je in assignment_3_1_3.m erachter vraagt 			of als je assignment_3_1_2.m apart runt 		- Deze code overgenomen van assignment_3_1_1.m 	--> Zie vorige commit om de aanpassingen te zien, was vergeten de nieuwe file te adden
    	* Assignment_3_1_3.m
    		- Begonnen met code schrijven
    		- Plot nu de opgenomen data
    			--> Je ziet dat het veel nerveuzer is, maar het blijft wel alles goed tracken
    		- Nog geen idee hoe je de force disturbance in de theoretische transferfunctie inbrengt
    			--> Moeten we best eens samen naar kijken

---
+++ Matlab_calculations/assignment_3_1_3.m
@@ -0,0 +1,137 @@
+close all
+clear all
+clc
+clear
+
+%% Assignment_3_1_2.m opvragen
+show_figures = 0;   % assignment_3_1_1 doesn't display its figures, if set to zero, so works a bit faster
+show_figures2 = 0;  % assignment_3_1_2 doesn't display its figures, if set to zero, so works a bit faster
+assignment_3_1_2
+
+
+%% Parameters, getting data and calculations
+close all
+clc
+
+% no parameters of the controller are adapted, so we use the same names as
+% in the previous assignment.
+
+% compile file name and import data
+rec = readlog('log_gpio_vc_incl_up.xml');
+
+% raw input data - these are sampled at a non-uniform rate!
+
+t_input = rec.getData('time');
+speed1_des = rec.getData('speed1_des');
+speed1_act = rec.getData('speed1_act');
+control_signal1 = rec.getData('control_signal1');
+speed2_des = rec.getData('speed2_des');
+speed2_act = rec.getData('speed2_act');
+control_signal2 = rec.getData('control_signal2');
+v_input = rec.getData('voltage');
+
+
+% Interpolate the input data to uniform timesteps 
+t = (t_input(1):Ts*1e3:t_input(1)+(length(t_input)-1)*Ts*1e3)'; % the equivalent of t_input if Ts were truly uniform
+v = v_input;                                                    % v doesn't have to be interpolated because the Arduino does a zoh                  
+speed1_des = interp1(t_input,speed1_des,t);                     % values of desired speed of motor 1 at the corresponding times
+speed1_act = interp1(t_input,speed1_act,t);                     % values of actual speed of motor 1 at the corresponding times
+control_singal1 = interp1(t_input,control_signal1,t);           % values of control signal of motor 1 at the corresponding times
+speed2_des = interp1(t_input,speed2_des,t);                     % values of desired speed of motor 2 at the corresponding times
+speed2_act = interp1(t_input,speed2_act,t);                     % values of actual speed of motor 2 at the corresponding times
+control_singal2 = interp1(t_input,control_signal2,t);           % values of control signal of motor 2 at the corresponding times
+t = 1e-3*(t - t_input(1));                                      % set t to be in s and start at 0 for convenience
+
+%% Plotting data of velocity with constant force disturbance
+% The cart was set on an inclination and both motors were set to the same
+% velocity, in order to let the cart drive straight forward.
+
+% Calculating simulations
+speed1_PIcomp_fb = lsim(sys_enc1_PIcomp_fb,speed1_des,t,':');
+speed2_PIcomp_fb = lsim(sys_enc2_PIcomp_fb,speed2_des,t,':');
+
+% clipping data to get limited time view
+t_start = 0;
+t_stop = 5;
+
+% Plotting data speed motor 1
+if show_figures2 == 1
+    figure('name', 'Comparison speed motor 1 with force disturbance')
+    subplot(2,2,1)
+    plot(t, speed1_des);
+    hold on
+    plot(t, speed1_act,'--');
+    hold off
+    xlabel('t [s]')
+    ylabel('speed [?]')
+    axis([t_start t_stop -inf inf]);
+    legend('desired speed','actual speed','location','northwest')
+    title('speed comparison of motor 1')
+    subplot(2,2,2)
+    plot(t, speed1_des);
+    hold on
+    plot(t, speed1_PIcomp_fb,'--');
+    hold off
+    xlabel('t [s]')
+    ylabel('speed [?]')
+    axis([t_start t_stop -inf inf]);
+    legend('desired speed','simulated speed','location','northwest')
+    title('speed comparison of motor 1')
+    subplot(2,2,3)
+    plot(t, speed1_act-speed1_des);
+    hold on
+    plot(t, speed1_PIcomp_fb-speed1_des,'--');
+    hold off
+    xlabel('t [s]')
+    ylabel('\Deltaspeed [?]')
+    axis([t_start t_stop -inf inf]);
+    legend('error actual','error simulated')
+    title('error between speed and desired speed')
+    subplot(2,2,4)
+    plot(t, control_signal1);
+    xlabel('t [s]')
+    ylabel('control signal 1 [mV]')
+    axis([t_start t_stop -inf inf]);
+    title('control signal of motor 1')
+end
+
+% Plotting data speed motor 2
+if show_figures2 == 1
+    figure('name', 'Comparison speed motor 2 with force disturbance')
+    subplot(2,2,1)
+    plot(t, speed2_des);
+    hold on
+    plot(t, speed2_act,'--');
+    hold off
+    xlabel('t [s]')
+    ylabel('speed [?]')
+    axis([t_start t_stop -inf inf]);
+    legend('desired speed','actual speed','location','northwest')
+    title('speed comparison of motor 2')
+    subplot(2,2,2)
+    plot(t, speed2_des);
+    hold on
+    plot(t, speed2_PIcomp_fb,'--');
+    hold off
+    xlabel('t [s]')
+    ylabel('speed [?]')
+    axis([t_start t_stop -inf inf]);
+    legend('desired speed','simulated speed','location','northwest')
+    title('speed comparison of motor 2')
+    subplot(2,2,3)
+    plot(t, speed2_act-speed2_des);
+    hold on
+    plot(t, speed2_PIcomp_fb-speed2_des,'--');
+    hold off
+    xlabel('t [s]')
+    ylabel('\Deltaspeed [?]')
+    axis([t_start t_stop -inf inf]);
+    legend('error actual','error simulated')
+    title('error between speed and desired speed')
+    subplot(2,2,4)
+    plot(t, control_signal2);
+    xlabel('t [s]')
+    ylabel('control signal 2 [mV]')
+    axis([t_start t_stop -inf inf]);
+    title('control signal of motor 2')
+end
