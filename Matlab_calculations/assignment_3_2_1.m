e0e061b812b51a510545be5881573a49e8cadf49
BenVandenBroeck <4d428b0a@opayq.com>
2016-11-30 10:29:28 +0100

    Nieuwe files tussengezet en andere aangepast
    * Zie code
    * Aan opdracht 2 begonnen
    	--> zit precies wel een fout in, we hebben geen integrator meer ???

---
+++ Matlab_calculations/assignment_3_2_1.m
@@ -0,0 +1,39 @@
+if ~ exist('show_figures4', 'var')
+    close all
+    clear all
+    clc
+    clear
+    show_figures4 = 1;   % show_figures3 can be set to 0 in another file to suppress the figures in this code
+end
+
+%% Assignment_3_1_3.m opvragen
+show_figures = 0;   % assignment_3_1_1 doesn't display its figures, if set to zero, so works a bit faster
+show_figures2 = 0;  % assignment_3_1_2 doesn't display its figures, if set to zero, so works a bit faster
+show_figures3 = 0;  % assignment_3_1_3 doesn't display its figures, if set to zero, so works a bit faster
+assignment_3_1_3
+
+
+%% Calculating open loop
+close all
+clc
+
+sys_enc1_PIcomp_fb_c = feedback(sys_enc1_PIcomp_c,1);   % Feedback system of speed 1 in continuous time
+differentiator = tf([1,0],[0,1]);   % differentiator = s
+integrator = tf([0,1],[1,0]);       % integrator = 1/s
+sys_pos_1_open_c = series(differentiator,series(sys_enc1_PIcomp_fb_c,integrator));    % openloop system of position 1
+
+if show_figures4 == 1
+    figure('name','openloop bode diagram positie 1 continuous time')
+    bode(sys_pos_1_open_c)
+end
+
+sys_pos_1_open_d = c2d(sys_pos_1_open_c,Ts);
+
+if show_figures4 == 1
+    figure('name','openloop bode diagram positie 1 discrete time')
+    bode(sys_pos_1_open_d)
+end
+
+
+
+
---
+++ Matlab_calculations/assignment_3_2_2.m
@@ -0,0 +1,37 @@
+if ~ exist('show_figures5', 'var')
+    close all
+    clear all
+    clc
+    clear
+    show_figures5 = 1;   % show_figures3 can be set to 0 in another file to suppress the figures in this code
+end
+
+%% Assignment_3_1_3.m opvragen
+show_figures = 0;   % assignment_3_1_1 doesn't display its figures, if set to zero, so works a bit faster
+show_figures2 = 0;  % assignment_3_1_2 doesn't display its figures, if set to zero, so works a bit faster
+show_figures3 = 0;  % assignment_3_1_3 doesn't display its figures, if set to zero, so works a bit faster
+show_figures4 = 0;  % assignment_3_2_1 doesn't display its figures, if set to zero, so works a bit faster
+assignment_3_2_1
+
+
+%% Calculating open loop
+close all
+clc
+
+sys_pos_1_open_c;
+sys_pos_1_open_d;
+
+sys_pos_1_open_c_fb = feedback(sys_pos_1_open_c,1);
+sys_pos_1_open_d_fb = feedback(sys_pos_1_open_c,1);
+
+if show_figures5 == 1
+    figure('name','closed loop bode diagram positie 1 continuous time')
+    bode(sys_pos_1_open_c_fb)
+end
+
+sys_pos_1_open_d = c2d(sys_pos_1_open_c,Ts);
+
+if show_figures5 == 1
+    figure('name','closed loop bode diagram positie 1 discrete time')
+    bode(sys_pos_1_open_d_fb)
+end
