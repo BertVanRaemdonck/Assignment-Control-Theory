f86c5afae4fa925dfc3f2cfc9c4cb059a9698d37
BenVandenBroeck <4d428b0a@opayq.com>
2016-12-22 12:09:21 +0100

    Experiment 6
    * Benadert nu pure dead reckoning
    	--> dus niet beter, maar het is niet meer onstabiel :)

--- KalmanFilter-Student/robot.cpp
+++ KalmanFilter-Student/robot.cpp
@@ -204,7 +204,7 @@ void Robot::resetNavigationController()
   // still need to fill in!
   _nav.setCartParameters(0.1695/2.0); // own code: measured value for a
   const float Kfb[2][3] { {0.5   ,      0,          0},
-                          {     0, 2.0   , 2.0       }
+                          {     0, 0.1   , 1.0       }
                         };
   _nav.setFeedbackGainMatrix(NavigationController::K_t(Kfb));
 }
